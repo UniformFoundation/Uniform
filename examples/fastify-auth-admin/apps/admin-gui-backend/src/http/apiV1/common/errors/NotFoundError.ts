5ff36f9d73686e8d523741dde45f0be923b6e536
Ilya Yurchenko <ilyha19@live.ru>
2023-02-27 10:51:30 +0300

    remove repos from example

--- examples/fastify-auth-admin/apps/admin-gui-backend/src/http/apiV1/common/errors/NotFoundError.ts
+++
@@ -1,9 +0,0 @@
-import { HttpError } from './HttpError';
-
-export class NotFoundError extends HttpError {
-    constructor(cause: unknown) {
-        super(404, 'The requested resource could not be found', cause);
-
-        this.name = 'Error 404';
-    }
-}
--- examples/fastify-auth-admin/apps/admin-gui-backend/src/http/apiV1/common/pagination/index.ts
+++
@@ -1,2 +0,0 @@
-// TODO: https://gitlab.com/greensight/magnit/admin-gui/admin-gui-backend/-/tree/dev/app/Http/ApiV1/Support/Pagination
-export {};
